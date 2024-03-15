962b85a03fb938ddb4341d39871349d0c47e891b
Joanna Wang <joannawang@Joannas-MacBook-Air.local>
2024-03-15 16:42:14 -0700

    app download

--- src/ListDisplay.jsx
+++ src/ListDisplay.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { List, ListItem, ListItemText, ImageList, ImageListItem, Typography, ImageListItemBar } from '@mui/material';
+import { List, ListItem, ListItemText, ImageList, ImageListItem, Typography, ImageListItemBar, Grid, Item } from '@mui/material';
 
 const ListDisplay = ({ images }) => {
   const totalQuantity = images.reduce((acc, curr) => acc + curr.quantity, 0);
 
   return (
     <div>
-      <Typography variant="h6">List</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={10}>
+        <img src="./Deck-Detect-3-15-2024.png" alt="Deck Detect" style={{ height: '100%', width: '30%' }} />
+        
+        </Grid>
+        <Grid item xs={2}>
+        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px' }}>
+          <a href="URL_FOR_DOWNLOAD_1">
+            <img src="./1.png" alt="Apple" style={{ cursor: 'pointer', marginBottom: '10px', width: '50px', height: 'auto' }} />
+          </a>
+          <a href="URL_FOR_DOWNLOAD_2">
+            <img src="./2.png" alt="Android" style={{ cursor: 'pointer', width: '50px', height: 'auto' }} />
+          </a>
+        </div>
+        </Grid>
+      </Grid>
+        
       
       <ImageList sx={{ width: 1300 }} cols={6} rowHeight={300}>
         {images.map((item) => (
