28789eba883bc6855e8dfe1868a3beb4cc7dd735
tianma <838212482@qq.com>
2024-03-17 00:05:33 -0700

    update web UI and set adjust grid

--- src/ListDisplay.jsx
+++ src/ListDisplay.jsx
@@ -1,83 +1,74 @@
 import React from 'react';
-import { List, ListItem, ListItemText, ImageList, ImageListItem, Typography, ImageListItemBar, useMediaQuery, useTheme } from '@mui/material';
+import { Typography, Grid, useMediaQuery, useTheme, ImageList, ImageListItem, ImageListItemBar, Box } from '@mui/material';
 import './ListDisplay.css'
+
 const ListDisplay = ({ images }) => {
   const totalQuantity = images.reduce((acc, curr) => acc + curr.quantity, 0);
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const cols = isMobile ? 2 : 4;
 
-  // Determine the number of columns based on whether the device is mobile
+  // Calculate row height dynamically based on the tallest image in each row
+  const rowHeights = images.reduce((acc, curr, index) => {
+    if (index % cols === 0) {
+      acc.push(0);
+    }
+    acc[acc.length - 1] = Math.max(acc[acc.length - 1], curr.height);
+    return acc;
+  }, []);
+  const rowHeight = Math.max(...rowHeights);
 
-  const imageSize = isMobile ? '33.3%' : '100%';
-  const cols = isMobile ? 3 : 6;
-  const rowHeight = isMobile ? 200 : 300
-  // Function to determine the number of columns based on the screen width
-  // const getNumCols = () => {
-  //   const screenWidth = window.innerWidth;
-  //   if (screenWidth >= 1300) {
-  //     return 6;
-  //   } else if (screenWidth >= 900) {
-  //     return 4;
-  //   } else if (screenWidth >= 600) {
-  //     return 3;
-  //   } else {
-  //     return 2;
-  //   }
-  // };
+  // Calculate gap between rows and columns
+  const gap = isMobile ? 8 : 16;
+
+  // Adjust container width and gap for larger screens
+  const containerWidth = isMobile ? '100%' : '90%';
+  const largeScreenGap = isMobile ? gap : 24; // Larger gap for larger screens
 
   return (
-    <div>
-      <Grid container spacing={2}>
-        <Grid item xs={10}>
-        <img src="./Deck-Detect-3-15-2024.png" alt="Deck Detect" style={{ height: '100%', width: '30%' }} />
+  <div style={{ margin: '0 auto', width: containerWidth }}>
+    <Box sx={{ mb: 4 }}>
+<Typography variant={isMobile ? "h3" : "h2"} align="center" gutterBottom style={{ fontFamily: 'Thuast', color: 'white', marginTop: '20px', marginBottom: '0', padding: '0' }}>Deck Detect</Typography>
+    </Box>
 
-        </Grid>
-        <Grid item xs={2}>
-        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px' }}>
-          <a href="URL_FOR_DOWNLOAD_1">
-            <img src="./1.png" alt="Apple" style={{ cursor: 'pointer', marginBottom: '10px', width: '50px', height: 'auto' }} />
-          </a>
-          <a href="URL_FOR_DOWNLOAD_2">
-            <img src="./2.png" alt="Android" style={{ cursor: 'pointer', width: '50px', height: 'auto' }} />
+
+    <ImageList sx={{ width: '100%', height: 'auto' }} cols={cols} rowHeight={rowHeight} gap={isMobile ? gap : largeScreenGap}>
+      {images.map((item) => (
+        <ImageListItem key={item.image_url}>
+          <img
+            src={`${item.image_url}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${item.image_url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt={item["card name"]}
+            loading="lazy"
+            style={{ width: '100%', height: 'auto' }}
+          />
+          <ImageListItemBar
+            title={`${item.quantity}`}
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+      <Grid item>
+        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px', marginRight: '10px', marginBottom: '0', paddingBottom: '0' }}>
+          <a href="https://apps.apple.com/us/app/deck-detect/id6476965574?platform=iphone">
+            <img src="./1.png" alt="Apple" style={{ cursor: 'pointer', marginTop: '10px', width: 'auto', height: '60px', marginBottom: '0', paddingBottom: '0' }} />
           </a>
         </div>
-        </Grid>
       </Grid>
+      <Grid item>
+        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '10px', marginRight: '20px', marginBottom: '0', paddingBottom: '0' }}>
+          <a href="URL_FOR_DOWNLOAD_2">
+            <img src="./2.png" alt="Android" style={{ cursor: 'pointer', marginTop: '10px', width: 'auto', height: '86px', marginBottom: '0', paddingBottom: '0' }} />
+          </a>
+        </div>
+      </Grid>
+    </Grid>
+    {/*<Typography variant="body1">totalQuantity: {totalQuantity}</Typography>*/}
+  </div>
+);
 
-      
-      <ImageList sx={{ width: isMobile ? '95%': 1300, height: isMobile ? 950 : 1000 }} cols={cols} rowHeight={rowHeight}>
-        {images.map((item) => (
-          <ImageListItem key={item.image_url}
-          //                sx={{
-          //   width: isMobile ? '33.3%' : 'auto', // Smaller width for ImageListItem on mobile
-          //   height: 'auto',
-          //   '& img': { // Targeting the image tag directly for additional style control
-          //     width: '100%', // Ensure the image fills the container
-          //     height: 'auto'  // Maintain aspect ratio
-          //   }
-          // }}
-          >
-            <img
-              src={`${item.image_url}?w=164&h=164&fit=crop&auto=format`}
-              srcSet={`${item.image_url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-              alt={item["card name"]}
-              loading="lazy"
-            />
-            <ImageListItemBar
-              title={`${item.quantity}`}
-              sx={{
-                // Adjust styles to place the quantity number on the right bottom
-                // background:
-                //   'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-              }}
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
-      <Typography variant="body1">totalQuantity: {images.length}</Typography>
-    </div>
-  );
 };
 
 export default ListDisplay;
